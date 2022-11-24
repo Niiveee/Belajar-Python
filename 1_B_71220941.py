d4acf146b7117341b20b96ef27553c4adb6bbe25
niivee <7122050@student.ukdw.ac.id>
2022-11-24 11:38:39 +0700

    https://github.com/Niiveee/Belajar-Python.git

---
+++ 1_B_71220941.py
@@ -0,0 +1,4 @@
+print ("====== ALAT PENGHITUNG LINGKARAN SENTOT ===== ")
+a = int(input ("Masukan Diameter (Meter) : "))
+print("Jarak yang harus di tempuh :", 3,14*a)
+
---
+++ 3_B_71220941.py
@@ -0,0 +1,13 @@
+print ("====== CATATAN BELANJA ======")
+print ("====== DAFTAR 1 ======")
+thislist = ["sikat gigi", "odol", "shampoo", "sabun", "ciduk"]
+print("Daftar belanja 1",thislist)
+print ("====== DAFTAR 2 ======")
+thislist2 = ["Teh", "Gula", "Garam", "Micin", "kecap"]
+print ("Daftar belanja 2",thislist2)
+print ("Jawab dengan angka {1/2}")
+input ("Masukan pilihan =",)
+input ("Masukan nama item ke daftar 1:",)
+input ("Masukan Index yang ingin dirubah :",)
+input ("Masukan nama item ke daftar 2 :",)
+print
